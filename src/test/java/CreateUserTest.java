37288779a16606d2f709757c728a6e94076e6e31
Olena Biliaieva <Olena.Biliaieva@davita.com>
2022-06-02 16:45:18 +0300

    Jenkins build

---
+++ src/test/java/CreateUserTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CreateUserTest {
+
+    @Test
+    public void createUserTest(){
+        System.out.println("User was created");
+        Assertions.assertTrue(true);
+    }
+}
